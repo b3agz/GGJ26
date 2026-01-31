0de0ce052d7d2d9b56648821f22f7476264c3716
WillowedForest <joe.phillips935@gmail.com>
2026-01-31 18:00:44 +0000

    timer done idk if its how you should do it in unity but it works

---
+++ Assets/01_Scripts/timer/Timer.cs
@@ -0,0 +1,67 @@
+using System;
+using TMPro;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    
+    /// <summary>
+    /// sorry for poor doc i'm just tring to gewt this done as fast as posible i hope this is the standerd
+    /// way of doing a timer in unity and c# if not i'm sorry -- willow
+    /// </summary>
+    
+    
+        [Tooltip("put how long in seconds")]
+        public float TimerDuration;
+    
+       [SerializeField] private TextMeshProUGUI timerText;
+        
+        private float timer;
+
+
+
+        void Start()
+        {
+            timer = TimerDuration;
+        }
+
+        void Update()
+        {
+            if (timer > 0)
+            {
+                timer -= Time.deltaTime;
+                UpdateTimerDisplay(timer);
+            }
+            else
+                Flash();
+        }
+
+        private void ResetTimer()
+        {
+            timer = TimerDuration;
+            UpdateTimerDisplay(timer);
+        }
+
+        private void UpdateTimerDisplay(float time)
+        {
+            float minutes = Mathf.FloorToInt(time / 60);
+            float seconds = Mathf.FloorToInt(time % 60);
+            
+            string currentTimer = string.Format("{0:D2}:{1:D2}", minutes.ToString(), seconds.ToString());
+            timerText.text = currentTimer;
+
+        }
+
+        private void Flash()
+        {
+            if(timer > 0)
+            {
+                timer = 0;
+                UpdateTimerDisplay(timer);
+                
+            }
+        }
+        
+        
+        
+}
