b75d9befd99587ec35000da05431b6010d1d3b77
WillowedForest <joe.phillips935@gmail.com>
2026-01-31 19:03:01 +0000

    made it so the sprite of the item is based on the data

--- Assets/01_Scripts/Interaction System/itemHolder.cs
+++ Assets/01_Scripts/Interaction System/itemHolder.cs
@@ -4,4 +4,13 @@ using UnityEngine;
 public class itemHolder : MonoBehaviour
 {
     public Item whatItemIs;
+    
+    private SpriteRenderer spriteRenderer;
+    
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        spriteRenderer.sprite = whatItemIs.Image;
+    }
 }
